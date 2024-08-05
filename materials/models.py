efc99a134c7ff4d5b3830cc6011ffa9c88712dc5
Emmanuil Galihanov <emikgal2507@gmail.com>
2024-08-05 21:30:35 +0500

    Создано приложение для курсов, написаны модели курса и урока

---
+++ materials/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+# Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Имя')
+    description = models.TextField(verbose_name='Описание')
+    preview = models.ImageField(upload_to='courses/', verbose_name='Превью')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Имя')
+    description = models.TextField(verbose_name='Описание')
+    preview = models.ImageField(upload_to='lessons/', verbose_name='Превью')
+    video_link = models.TextField(verbose_name='Ссылка на видео')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='Курс')
