a09aa3c66ae783fda8f0b13cca3ce09a1bd8a7e0
Emmanuil Galihanov <emikgal2507@gmail.com>
2024-08-08 18:41:22 +0500

    Создана модель платежей и сериализатор для нее

--- users/views.py
+++ users/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 
-from users.models import User
-from users.serializes import UserSerializer
+from users.models import User, Payment
+from users.serializes import UserSerializer, PaymentSerializer
 
 
 # Create your views here.
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    serializer_class = PaymentSerializer
+    queryset = Payment.objects.all()
