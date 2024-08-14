6e7c260401510108d9968e21ab65a83fe06571f2
Emmanuil Galihanov <emikgal2507@gmail.com>
2024-08-14 21:49:34 +0500

    Создана группа модераторов. Описаны права работы с курсами и уроками. Добавлена проверка прав в контроллеры.

--- materials/views.py
+++ materials/views.py
@@ -3,6 +3,7 @@ from rest_framework.permissions import IsAuthenticated
 
 from materials.models import Course, Lesson
 from materials.serializes import CourseSerializer, LessonSerializer
+from users.permissions import IsModerator
 
 
 # Create your views here.
@@ -12,31 +13,38 @@ class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     permission_classes = [IsAuthenticated]
 
+    def get_permissions(self):
+        if self.action in ['retrieve', 'update']:
+            self.permission_classes = [IsModerator]
+        elif self.action in ['create', 'destroy']:
+            self.permission_classes = [~IsModerator]
+        return super().get_permissions()
+
 
 class LessonCreateAPIView(generics.CreateAPIView):
     serializer_class = LessonSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, ~IsModerator]
 
 
 class LessonListAPIView(generics.ListAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsModerator]
 
 
 class LessonRetrieveAPIView(generics.RetrieveAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsModerator]
 
 
 class LessonUpdateAPIView(generics.UpdateAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsModerator]
 
 
 class LessonDestroyAPIView(generics.DestroyAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, ~IsModerator]
