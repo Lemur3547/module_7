70a035b8a1eb5b054f673d84e49bb36704f41068
Emmanuil Galihanov <emikgal2507@gmail.com>
2024-08-20 18:30:30 +0500

    Добавлена валидация для ссылки на урок в модели урока.

--- materials/serializes.py
+++ materials/serializes.py
@@ -1,12 +1,14 @@
 from rest_framework import serializers
 
 from materials.models import Course, Lesson
+from materials.validators import VideoLinkValidator
 
 
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
+        validators = [VideoLinkValidator(field='video_link')]
 
 
 class CourseSerializer(serializers.ModelSerializer):
