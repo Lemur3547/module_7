7b0eec57e1cb7d9cfd8a218e01a3b51589ddb789
Emmanuil Galihanov <emikgal2507@gmail.com>
2024-08-24 00:24:58 +0500

    Подключена оплата курсов через stripe

---
+++ users/services.py
@@ -0,0 +1,27 @@
+import stripe
+
+stripe.api_key = "sk_test_51PqymIP32io8mk7XYu5ztjQj0484NL65d4xy6Cir7ZJJUSqloNhgUASzAMwHeq4gmiPNjXEum3gr63CEivirDOHq00sdKgOm9S"
+
+
+def create_product(product):
+    """Создает продукт"""
+    return stripe.Product.create(name=f"{product}")
+
+
+def create_price(price, product):
+    """Создает цену в stripe."""
+    return stripe.Price.create(
+        currency="rub",
+        unit_amount=price * 100,
+        product=product.get('id')
+    )
+
+
+def create_session(price):
+    """Создает сессию для оплаты"""
+    session = stripe.checkout.Session.create(
+        success_url="http://127.0.0.1:8000/",
+        line_items=[{"price": price.get('id'), "quantity": 1}],
+        mode="payment",
+    )
+    return session.get('id'), session.get('url'), session.get('payment_method_types')[0]
--- users/views.py
+++ users/views.py
@@ -1,11 +1,15 @@
+import datetime
+
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, generics
+from rest_framework.exceptions import ValidationError
 from rest_framework.filters import OrderingFilter
 from rest_framework.permissions import IsAuthenticated
 
 from users.models import User, Payment
 from users.permissions import IsSelfUser
 from users.serializes import SelfUserSerializer, AnotherUserSerializer, PaymentSerializer, UserRegisterSerializer
+from users.services import create_product, create_price, create_session
 
 
 # Create your views here.
@@ -52,3 +56,23 @@ class PaymentViewSet(viewsets.ModelViewSet):
     filter_backends = [DjangoFilterBackend, OrderingFilter]
     filterset_fields = ('course', 'lesson', 'payment_method')
     ordering_fields = ('date',)
+
+    def perform_create(self, serializer):
+        payment = serializer.save(user=self.request.user)
+        if payment.course is None and payment.lesson is None:
+            raise ValidationError('Выберите курс или урок для оплаты')
+        elif payment.course is not None and payment.lesson is not None:
+            raise ValidationError('Выберите только один курс или урок для оплаты')
+        else:
+            if payment.course:
+                product = create_product(payment.course)
+            else:
+                product = create_product(payment.lesson)
+        price = create_price(price=payment.summ, product=product)
+        session_id, session_url, payment_method = create_session(price)
+        payment.session_id = session_id
+        payment.payment_link = session_url
+        payment.date = datetime.datetime.now()
+        payment.payment_method = payment_method
+        payment.save()
+
